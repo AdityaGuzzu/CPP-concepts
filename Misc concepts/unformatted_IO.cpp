d68e48fea0b87f50c9e03a041d435f44858471ee
Gujju Aditya <guzzuaditya@gmail.com>
2022-07-21 18:13:33 +0530

    Added wrong reference variable initialisation and unformatted IO

---
+++ Misc concepts/unformatted_IO.cpp
@@ -0,0 +1,19 @@
+/*A program to demonstrate unformatted input and output streams in C++ */
+#include<iostream>
+using namespace std;
+int main()
+{
+	char c;
+	char constraint;
+	int limit;
+	char word[20];
+	cout<<"Enter the character: ";
+	cin>>c;
+	cout<<"\nEnter the size of the string: ";
+	cin>>limit;
+	cout<<"\nEnter the constraint character: ";
+	cin>>constraint;
+	cout<<"Now enter the string: ";
+	cin.getline(word,limit,constraint);
+	cout<<word;	
+}
---
+++ Pointers_And_Referance_Variables/ref_var_wrong_initialisation.cpp
@@ -0,0 +1,9 @@
+//A program to find more about initialising ref variables.
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x;
+	int &ref;		//reference variable
+	ref = x;		//Trying to assign x. This is illegal. Reference varibales must be initialised immediateltely after declaring
+}	
