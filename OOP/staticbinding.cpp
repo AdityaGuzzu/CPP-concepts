f643ea888b1bda2fe9383def21099b3d3d1c4a4e
Gujju Aditya <guzzuaditya@gmail.com>
2022-07-29 16:40:20 +0530

    added static binding file in OOP folder

--- OOP/staticbinding.cpp
+++ OOP/staticbinding.cpp
@@ -3,5 +3,24 @@
 using namespace std;
 class parent
 {
-    
+    public:
+    void welcome() {cout<<endl<<"Welcome to parent class";}   
 };
+
+class child: public parent
+{
+    public:
+    void welcome() {cout<<endl<<"Welcome to child class";}
+};
+
+int main()
+{
+    parent *ptr[] = 
+    {
+        new parent,
+        new child
+    };
+    ptr[0]->welcome();      //parent class' function
+    ptr[1]->welcome();      //child class' function. We see that the function defined
+                            //   in child class isn't called
+}
