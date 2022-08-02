398719d90cfca6ec3564f00c3ad1213429e2c337
Gujju Aditya <guzzuaditya@gmail.com>
2022-08-02 23:19:58 +0530

    new2.cpp added

---
+++ Pointers_And_Referance_Variables/new_2.cpp
@@ -0,0 +1,14 @@
+/* Using square brackets [] isntead of round brackets () */
+#include<iostream>
+using namespace std;
+int main()
+{
+    int size;
+    cout<<"Enter the size";
+    cin>>size;
+    int *ptr = new int[size];
+    for(int i=0;i<size;i++)
+    {
+        cout<<endl<<(int)ptr[i]         //The issue of size variable being printed is solved.
+    }
+}
