12abaca15f090e6adcf12bd480d1e786b8a0b446
Gujju Aditya <guzzuaditya@gmail.com>
2022-08-04 14:57:01 +0530

    Added vectors with pointer arrays

---
+++ Pointers_And_Referance_Variables/new_3.cpp
@@ -0,0 +1,26 @@
+/* Create a vector of pointers */
+#include<iostream>
+#include<vector>
+using namespace std;
+class base
+{
+    private:
+    int x, y;
+    public:
+    base(int x, int y): x(x), y(y) {cout<<"\nX = "<<x<<"\tY = "<<y;}
+    void get_val()
+    {
+        cout<<endl<<"X = "<<x<<"\tY ="<<y;
+    }
+};
+
+int main()
+{
+    //IDK why but I am only getting the output of the first object created.
+    std::vector<base *> base_ptrs;
+    base_ptrs[0] = new base(10,11);
+    base_ptrs[1] = new base(12,13);
+    base_ptrs[2] = new base(14,15);
+    base_ptrs[1]->get_val();
+    base_ptrs[2]->get_val();
+}
