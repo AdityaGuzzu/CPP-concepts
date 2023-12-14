3e14088f601fb2a2bf41dc0f9ee7c863dc462ff3
aditya guzzu <guzzuaditya@gmail.com>
2023-12-14 22:34:44 +0530

    Added research_paper folder

--- Misc_Concepts/Rand()/randnumber.cpp
+++ Misc_Concepts/Rand()/randnumber.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
 #include<cstdlib>
+#include<time.h>
 using namespace std;
 int main()
 {
+    srand(time(NULL));
     for(int x = 0; x<10; x++)
     {
         cout<<endl<<rand();       //generates random numbers
